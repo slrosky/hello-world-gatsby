12ceb88740d47bea3b07e3abdcc659a226a4303b
Sarah Rosky <sarahrosky@gmail.com>
2021-05-18 07:44:31 -0700

    end of tutorial pt 1

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "../components/header"
+
+const About = () => {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" />
+      <Header headerText="It's pretty cool" />
+
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
+
+export default About
